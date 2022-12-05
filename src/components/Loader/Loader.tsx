e274885e1d57b03e05f6823a73a102b5020255ad
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-12-05 16:21:48 +0100

    create loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+import animation from "lotties/animated-dots.json"
+
+import data from "data/user.json"
+
+import * as Styled from "./Loader.styles"
+
+interface Props {
+  item: string
+}
+
+const Loader = ({ item }: Props) => (
+  <div>
+    <Styled.Animation animationData={animation} loop={true} />
+    <Styled.Text>
+      {`${data.infos.name}'s ${item} are being fetched...`}
+    </Styled.Text>
+  </div>
+)
+
+export default Loader
