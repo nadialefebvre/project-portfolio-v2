f46e7b70166c61c1c7c2627122c7a5e7bdf10dd8
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-12 17:52:41 +0100

    simplify loader component

--- src/components/Loader/Loader.styles.tsx
+++ src/components/Loader/Loader.styles.tsx
@@ -2,21 +2,7 @@ import styled from "styled-components/macro"
 
 import Lottie from "lottie-react"
 
-import { media } from "utils/media-queries"
-
 export const Animation = styled(Lottie)`
   width: 50%;
   margin: 0 auto;
-`
-
-export const Text = styled.p`
-  font-size: 17px;
-  line-height: 171%;
-  text-align: center;
-  ${media.tabletUp} {
-    font-size: 24px;
-  }
-  ${media.desktopUp} {
-    line-height: 167%;
-  }
 `
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,21 +2,8 @@ import React from "react"
 
 import animation from "data/animated-dots.json"
 
-import data from "data/user.json"
-
 import * as Styled from "./Loader.styles"
 
-interface Props {
-  item: string
-}
-
-const Loader = ({ item }: Props) => (
-  <div>
-    <Styled.Animation animationData={animation} loop={true} />
-    <Styled.Text>
-      {`${data.infos.name}'s ${item} are being fetched...`}
-    </Styled.Text>
-  </div>
-)
+const Loader = () => <Styled.Animation animationData={animation} loop={true} />
 
 export default Loader
