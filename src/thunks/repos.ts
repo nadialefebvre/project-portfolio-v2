17d4cf639eabb2eb4ee6f87e5930733107ec5d15
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-12 10:15:15 +0100

    Revert "remove not needed code"
    This reverts commit b989881590ee847e86dabdcadc73765ef72f0292.

--- src/thunks/repos.ts
+++ src/thunks/repos.ts
@@ -1,3 +1,32 @@
+/*
+import repos from "reducers/repos"
+
+import { AppDispatch } from "store/types"
+
+import { githubQuery } from "utils/github-query"
+
+import { useQuery } from "@apollo/client"
+import { useFetchData } from 'thunks/test';
+
+export const fetchRepos = () => {
+  return (dispatch: AppDispatch) => {
+    const { loading, error, data } = useFetchData()
+
+    if (loading) {
+      console.log("I am loading")
+    }
+
+    if (error) {
+      console.log("I am an error")
+    }
+
+    dispatch(repos.actions.setAllRepos(data.search.nodes))
+    dispatch(repos.actions.setPinnedRepos(data.user.pinnedItems.nodes))
+    dispatch(repos.actions.setIsLoading(false))
+  }
+}
+*/
+
 import { useFetchData } from 'thunks/test';
 import repos from "reducers/repos"
 import { AppDispatch } from "store/types"
@@ -7,7 +36,6 @@ export const fetchRepos = () => {
     const { loading, error, data } = useFetchData()
 
     if (loading) {
-      dispatch(repos.actions.setIsLoading(true))
       console.log("I am loading")
     }
 
