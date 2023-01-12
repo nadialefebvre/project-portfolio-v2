100d10d8aa977d1b5538351041c234281ea0bd6e
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-12 18:02:07 +0100

    use apolloclient for fetching

--- src/thunks/repos.ts
+++ src/thunks/repos.ts
@@ -1,29 +1,22 @@
 import repos from "reducers/repos"
-
+import { client } from "App/App"
 import { AppDispatch } from "store/types"
-
 import { githubQuery } from "utils/github-query"
 
-import { githubEndpoint, GITHUB_TOKEN } from "constants/github"
-
 export const fetchRepos = () => {
-  return (dispatch: AppDispatch) => {
-    dispatch(repos.actions.setIsLoading(true))
-    fetch(githubEndpoint, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-      body: JSON.stringify({
-        query: githubQuery,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        dispatch(repos.actions.setAllRepos(res.data.search.nodes))
-        dispatch(repos.actions.setPinnedRepos(res.data.user.pinnedItems.nodes))
-        dispatch(repos.actions.setIsLoading(false))
-      })
-      .catch((err) => console.log(err))
+  return async (dispatch: AppDispatch) => {
+    const { data, loading, error } = await client.query({ query: githubQuery })
+
+    if (loading) {
+      dispatch(repos.actions.setIsLoading(true))
+    }
+
+    if (error) {
+      dispatch(repos.actions.setError(error))
+    }
+
+    dispatch(repos.actions.setAllRepos(data.search.nodes))
+    dispatch(repos.actions.setPinnedRepos(data.user.pinnedItems.nodes))
+    dispatch(repos.actions.setIsLoading(false))
   }
 }
