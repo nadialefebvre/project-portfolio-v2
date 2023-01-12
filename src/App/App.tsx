1afbfd44e2b4b199e46121d824e5605ddbfe2ff2
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-12 17:51:35 +0100

    add apolloclient

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,6 +2,13 @@ import React from "react"
 
 import { Provider as ReduxProvider } from "react-redux"
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  // gql,
+} from "@apollo/client"
+
 import { ThemeProvider } from "styled-components"
 
 import { store } from "store/configureStore"
@@ -21,20 +28,32 @@ import {
 
 import { GlobalStyle } from "App/App.styles"
 
+import { githubEndpoint, GITHUB_TOKEN } from "constants/github"
+
+export const client = new ApolloClient({
+  uri: githubEndpoint,
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+  },
+  cache: new InMemoryCache(),
+})
+
 const App = () => (
-  <ReduxProvider store={store}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <Intro />
-      <Tech />
-      <Projects />
-      <Thoughts />
-      <Skills />
-      <ForMore />
-      <Footer />
-    </ThemeProvider>
-  </ReduxProvider>
+  <ApolloProvider client={client}>
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <Intro />
+        <Tech />
+        <Projects />
+        <Thoughts />
+        <Skills />
+        <ForMore />
+        <Footer />
+      </ThemeProvider>
+    </ReduxProvider>
+  </ApolloProvider>
 )
 
 export default App
