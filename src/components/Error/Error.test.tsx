0eb2a7bb6d22334060472dd6089a1c154d802d6f
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-18 15:22:24 +0100

    add test for Error component

---
+++ src/components/Error/Error.test.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+import { cleanup, render, screen } from "@testing-library/react"
+
+import Error from "./Error"
+
+afterEach(cleanup)
+
+describe("Error component", () => {
+  test("should render the correct text for item and error", () => {
+    const item = "projects"
+    const error = "404 Not Found"
+    render(<Error item={item} error={error} />)
+    const itemText = screen.getByText(
+      `There's an issue with the API fetching the ${item}:`
+    )
+    const errorText = screen.getByText(`Error: ${error}`)
+    const refreshText = screen.getByText(
+      "Please refresh the page, or try later."
+    )
+    expect(itemText).toBeInTheDocument()
+    expect(errorText).toBeInTheDocument()
+    expect(refreshText).toBeInTheDocument()
+  })
+
+  test("should not render error message when error is empty", () => {
+    const item = "projects"
+    const error = ""
+    render(<Error item={item} error={error} />)
+    expect(screen.queryByText("An error occurred")).toBeNull()
+  })
+})
