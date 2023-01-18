2d550ab82fa07912763fcb6be61ccf66d986077a
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-18 15:24:26 +0100

    add test for Tags component

---
+++ src/components/Tags/Tags.test.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+import { cleanup, render, screen } from "@testing-library/react"
+
+import Tags from "./Tags"
+
+afterEach(cleanup)
+
+describe("Tags", () => {
+  const list = ["react", "javascript", "frontend"]
+  const fixTopic = (item: string) => item.toUpperCase()
+  const isItalic = true
+
+  test("should render the tags correctly", () => {
+    render(<Tags list={list} fixTopic={fixTopic} isItalic={isItalic} />)
+    list.forEach((item) => {
+      const tag = screen.getByText(`#${fixTopic(item)}`)
+      expect(tag).toBeInTheDocument()
+    })
+  })
+
+  test("should apply styles correctly", () => {
+    render(<Tags list={list} fixTopic={fixTopic} isItalic={isItalic} />)
+    list.forEach((item) => {
+      const tag = screen.getByText(`#${fixTopic(item)}`)
+      expect(tag).toHaveStyle("font-style: italic")
+    })
+  })
+
+  test("should not render any tag when list is empty", () => {
+    render(<Tags list={[]} fixTopic={fixTopic} isItalic={true} />)
+    expect(screen.queryByText("#")).toBeNull()
+  })
+})
