d215ef14afdccb1ac8c3bd56a172a2534098e881
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-18 16:57:33 +0100

    fix small details

--- src/components/Projects/FeaturedProject/FeaturedProject.test.tsx
+++ src/components/Projects/FeaturedProject/FeaturedProject.test.tsx
@@ -3,9 +3,9 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 
 import FeaturedProject from "./FeaturedProject"
-import { RepoInterface } from "components/Projects"
 import { longNameInTitleCase } from "utils/project-name"
 import data from "mapping-data/user.json"
+import { RepoInterface } from "components/Projects"
 
 const project: RepoInterface = {
   id: "123",
@@ -32,12 +32,12 @@ const project: RepoInterface = {
   },
 }
 
-describe("<FeaturedProject />", () => {
+describe("FeaturedProject component", () => {
   test("should render the project image, title, description and tags", () => {
     render(<FeaturedProject project={project} />)
-    const image = screen.getByAltText(/mocked project/i)
+    const image = screen.getByAltText(longNameInTitleCase(project.name))
     const title = screen.getByText(/frontend web app/i)
-    const description = screen.getByText(/This is a mocked project/i)
+    const description = screen.getByText(project.description)
     const tags = screen.getAllByText(/topic/i)
     expect(image).toBeInTheDocument()
     expect(title).toBeInTheDocument()
@@ -48,8 +48,11 @@ describe("<FeaturedProject />", () => {
   test("should render the correct project image source", () => {
     render(<FeaturedProject project={project} />)
     const image: HTMLImageElement = screen.getByRole("img")
+    const username = data.infos.usernameGitHub
+    const projectName = project.name
+    const defaultBranch = project.defaultBranchRef?.name
     expect(image.src).toBe(
-      `https://raw.githubusercontent.com/${data.infos.usernameGitHub}/${project.name}/${project.defaultBranchRef?.name}/screenshot.jpg`
+      `https://raw.githubusercontent.com/${username}/${projectName}/${defaultBranch}/screenshot.jpg`
     )
     expect(image.alt).toBe(longNameInTitleCase(project.name))
   })
