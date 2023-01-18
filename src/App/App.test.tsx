6ed2d2b863bb4ba8b104d24fe80308bed16f9a78
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-18 15:22:10 +0100

    add test for App component

---
+++ src/App/App.test.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+
+import { cleanup, render, screen } from "@testing-library/react"
+
+import App from "./App"
+
+afterEach(cleanup)
+
+describe("App component", () => {
+  test("renders Header component in App", () => {
+    render(<App />)
+    const textInHeader = screen.getByText(/^portfolio/i)
+    expect(textInHeader).toBeInTheDocument()
+  })
+
+  test("should render Intro component in App", () => {
+    render(<App />)
+    const element = screen.getByText(/^Hello!/)
+    expect(element).toBeInTheDocument()
+  })
+
+  test("should render Tech component in App", () => {
+    render(<App />)
+    const techSection = screen.getByTestId("Tech")
+    expect(techSection).toBeInTheDocument()
+    expect(techSection.tagName).toBe("SECTION")
+  })
+
+  test("should render Projects component in App", () => {
+    render(<App />)
+    const projectsSection = screen.getByTestId("Featured projects")
+    expect(projectsSection).toBeInTheDocument()
+    expect(projectsSection.tagName).toBe("SECTION")
+  })
+
+  test("should render Thoughts component in App", () => {
+    render(<App />)
+    const thoughtsSection = screen.getByTestId("My thoughts")
+    expect(thoughtsSection).toBeInTheDocument()
+    expect(thoughtsSection.tagName).toBe("SECTION")
+  })
+
+  test("should render Skills component in App", () => {
+    render(<App />)
+    const skillsSection = screen.getByTestId("Skills")
+    expect(skillsSection).toBeInTheDocument()
+    expect(skillsSection.tagName).toBe("SECTION")
+  })
+
+  test("should render ForMore component in App", () => {
+    render(<App />)
+    const forMoreSection = screen.getByTestId("For more")
+    expect(forMoreSection).toBeInTheDocument()
+    expect(forMoreSection.tagName).toBe("SECTION")
+  })
+
+  test("should render Footer component in App", () => {
+    render(<App />)
+    const textInFooter = screen.getByText(/^Contact/i)
+    expect(textInFooter).toBeInTheDocument()
+  })
+})
