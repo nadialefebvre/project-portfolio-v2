b2cfb0ea7ba36e3c22fc31a81efb7a8cc8111027
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-18 16:58:36 +0100

    add test for OtherThought component

---
+++ src/components/Thoughts/OtherThought/OtherThought.test.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+import { render, screen } from "@testing-library/react"
+
+import OtherThought from "./OtherThought"
+import { formattedDate } from "utils/formattedDate"
+import { StoryInterface } from "components/Thoughts"
+
+const thought: StoryInterface = {
+  title: "I wrote a mocked story",
+  pubDate: "2023-01-09 15:27:24",
+  link: "https://nadialefebvredev.medium.com/i-wrote-a-mocked-story-123?source=rss-4a366fcaabd4------2",
+  guid: "https://medium.com/p/123",
+  author: "Nadia Lefebvre",
+  thumbnail: "https://cdn-images-1.medium.com/max/1024/0*MWZNUI_GoDUfvANG",
+  description:
+    '\n<figure><img alt="Rock art photo by Rabah Al Shammary on Unsplash" src="https://cdn-images-1.medium.com/max/1024/0*MWZNUI_GoDUfvANG"></figure><p>This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me.</p>\n<img src="https://medium.com/_/stat?event=post.clientViewed&amp;referrerSource=full_rss&amp;postId=620c998688fe" width="1" height="1" alt="">\n',
+  content:
+    '\n<figure><img alt="Rock art photo by Rabah Al Shammary on Unsplash" src="https://cdn-images-1.medium.com/max/1024/0*MWZNUI_GoDUfvANG"></figure><p>This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me.</p>\n<img src="https://medium.com/_/stat?event=post.clientViewed&amp;referrerSource=full_rss&amp;postId=620c998688fe" width="1" height="1" alt="">\n',
+  enclosure: {},
+  categories: ["topic-one", "topic-two", "topic-three"],
+}
+
+describe("FeaturedThought component", () => {
+  test("should render the thought image, title, text and tags", () => {
+    render(<OtherThought thought={thought} />)
+    const date = screen.getByText(`${formattedDate(thought.pubDate)}.`)
+    const title = screen.getByText(thought.title)
+    expect(date).toBeInTheDocument()
+    expect(title).toBeInTheDocument()
+  })
+})
