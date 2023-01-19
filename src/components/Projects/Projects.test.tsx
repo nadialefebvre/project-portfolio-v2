334343597fea9e6fa2f68dcf80d7420e38195d41
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-19 13:47:38 +0100

    add test for Projects component

---
+++ src/components/Projects/Projects.test.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+import { render, screen } from "@testing-library/react"
+
+import { Projects } from "components/Projects"
+import { Provider as ReduxProvider } from "react-redux"
+import { store } from "store/configureStore"
+
+describe("Projects component", () => {
+  test("should render the component", () => {
+    const { container } = render(
+      <ReduxProvider store={store}>
+        <Projects />
+      </ReduxProvider>
+    )
+    expect(container).toBeInTheDocument()
+  })
+
+  test("should display loader when fetching data", () => {
+    render(
+      <ReduxProvider store={store}>
+        <Projects />
+      </ReduxProvider>
+    )
+    const loader = screen.getByTestId("animation")
+    expect(loader).toBeInTheDocument()
+  })
+
+  test("should display featured projects title", () => {
+    render(
+      <ReduxProvider store={store}>
+        <Projects />
+      </ReduxProvider>
+    )
+
+    const featuredProjects = screen.getByText("Featured projects")
+    expect(featuredProjects).toBeInTheDocument()
+  })
+})
