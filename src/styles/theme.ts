ed9f817b1f1ed7bd17cd78677d6f85ec05baa82b
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-19 12:45:01 +0100

    add interface to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,12 @@
-export const theme = {
+interface Theme {
+  white: string
+  lightGrey: string
+  primary: string
+  lightPrimary: string
+  secondary: string
+}
+
+export const theme: Theme = {
   white: "#ffffff",
   lightGrey: "#f4f4f4",
   primary: "#127c8a",
