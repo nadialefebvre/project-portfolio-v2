252d12335e4e9b5b32c815d44fb8a13e425f7e00
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-19 13:47:47 +0100

    add test for Thoughts component

---
+++ src/components/Thoughts/Thoughts.test.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+import { render, screen } from "@testing-library/react"
+
+import { Thoughts } from "components/Thoughts"
+import { Provider as ReduxProvider } from "react-redux"
+import { store } from "store/configureStore"
+
+describe("Thoughts component", () => {
+  test("should render the component", () => {
+    const { container } = render(
+      <ReduxProvider store={store}>
+        <Thoughts />
+      </ReduxProvider>
+    )
+    expect(container).toBeInTheDocument()
+  })
+
+  test("should display loader when fetching data", () => {
+    render(
+      <ReduxProvider store={store}>
+        <Thoughts />
+      </ReduxProvider>
+    )
+    const loader = screen.getByTestId("animation")
+    expect(loader).toBeInTheDocument()
+  })
+
+  test("should display featured thoughts title", () => {
+    render(
+      <ReduxProvider store={store}>
+        <Thoughts />
+      </ReduxProvider>
+    )
+
+    const featuredThoughts = screen.getByText("My thoughts")
+    expect(featuredThoughts).toBeInTheDocument()
+  })
+})
