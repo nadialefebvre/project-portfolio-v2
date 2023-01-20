0b9f356d27b1339856a7453b658073681a64804e
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-20 18:31:09 +0100

    move console.error to Error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -10,22 +10,30 @@ interface Props {
   error: string
 }
 
-const Error: React.FC<Props> = ({ title, extraTitle, text, error }) => (
-  <Section title={title} extraTitle={extraTitle}>
-    <Styled.IconBox>
-      <Styled.Icon />
-    </Styled.IconBox>
-    <Styled.Text>{text}</Styled.Text>
-    <Styled.Error>{`Error: ${error}`}</Styled.Error>
-    <Styled.Text>Please refresh the page, or try later.</Styled.Text>
-    <Styled.Text>
-      If the error remains, please{" "}
-      <a href="mailto:nadialefebvre.dev@hotmail.com?subject=Issue with your portfolio">
-        contact the developer
-      </a>
-      .
-    </Styled.Text>
-  </Section>
-)
+const Error: React.FC<Props> = ({ title, extraTitle, text, error }) => {
+  console.error(
+    "%cERROR =>",
+    "color: red; font-weight: bold; text-decoration: underline;",
+    error
+  )
+
+  return (
+    <Section title={title} extraTitle={extraTitle}>
+      <Styled.IconBox>
+        <Styled.Icon />
+      </Styled.IconBox>
+      <Styled.Text>{text}</Styled.Text>
+      <Styled.Error>{`Error: ${error}`}</Styled.Error>
+      <Styled.Text>Please refresh the page, or try later.</Styled.Text>
+      <Styled.Text>
+        If the error remains, please{" "}
+        <a href="mailto:nadialefebvre.dev@hotmail.com?subject=Issue with your portfolio">
+          contact the developer
+        </a>
+        .
+      </Styled.Text>
+    </Section>
+  )
+}
 
 export default Error
--- src/thunks/stories.ts
+++ src/thunks/stories.ts
@@ -9,11 +9,6 @@ export const fetchStories = () => {
       const json = await res.json()
       dispatch(stories.actions.setStories(json.items))
     } catch (error) {
-      console.error(
-        "%cERROR =>",
-        "color: red; font-weight: bold; text-decoration: underline;",
-        error
-      )
       dispatch(stories.actions.setError(error))
     } finally {
       dispatch(stories.actions.setIsLoading(false))
