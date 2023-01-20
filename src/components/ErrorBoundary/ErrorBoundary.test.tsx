05cf81c920537c3dc90496170fe84de182b997ad
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-20 18:54:13 +0100

    add cleanup

--- src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 
-import { screen, render } from "@testing-library/react"
+import { cleanup, screen, render } from "@testing-library/react"
 
 import ErrorBoundary from "./ErrorBoundary"
 
 describe("ErrorBoundary", () => {
+  afterEach(cleanup)
+
   it("renders children when there is no error", () => {
     render(
       <ErrorBoundary>
