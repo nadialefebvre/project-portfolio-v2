4ed903e21570ff4bfcd996580900cc99951196d0
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-20 18:31:55 +0100

    create ErrorBoundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+import { Error } from "components"
+
+class ErrorBoundary extends React.Component<any, any> {
+  state: any = {
+    error: null,
+  }
+
+  componentDidCatch(error: any) {
+    this.setState({
+      error: error.message,
+    })
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <Error
+          title={this.props.title}
+          extraTitle={this.props.extraTitle}
+          text="There's an issue with this component"
+          error={this.state.error}
+        />
+      )
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
