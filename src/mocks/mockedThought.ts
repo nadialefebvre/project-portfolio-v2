50cd6c76c9c29230ae25ca63fffe22505b2fbec6
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-23 15:05:05 +0100

    remove name from mock data

--- src/mocks/mockedThought.ts
+++ src/mocks/mockedThought.ts
@@ -3,14 +3,14 @@ import { StoryInterface } from "components/Thoughts"
 export const mockedThought: StoryInterface = {
   title: "I wrote a mocked story",
   pubDate: "2023-01-09 15:27:24",
-  link: "https://nadialefebvredev.medium.com/i-wrote-a-mocked-story-123?source=rss-4a366fcaabd4------2",
+  link: "https://username.medium.com/i-wrote-a-mocked-story-123?source=rss-4a366fcaabd4------2",
   guid: "https://medium.com/p/123",
-  author: "Nadia Lefebvre",
-  thumbnail: "https://cdn-images-1.medium.com/max/1024/0*MWZNUI_GoDUfvANG",
+  author: "User Name",
+  thumbnail: "https://cdn-images-1.medium.com/max/1024/0*MWZGHHGHANG",
   description:
-    '\n<figure><img alt="Rock art photo by Rabah Al Shammary on Unsplash" src="https://cdn-images-1.medium.com/max/1024/0*MWZNUI_GoDUfvANG"></figure><p>This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me.</p>\n<img src="https://medium.com/_/stat?event=post.clientViewed&amp;referrerSource=full_rss&amp;postId=620c998688fe" width="1" height="1" alt="">\n',
+    '\n<figure><img alt="Photo by Someone on Unsplash" src="https://cdn-images-1.medium.com/max/1024/0*MHHHHHoDUfvANG"></figure><p>This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me.</p>\n<img src="https://medium.com/_/stat?event=post.clientViewed&amp;referrerSource=full_rss&amp;postId=620c998688fe" width="1" height="1" alt="">\n',
   content:
-    '\n<figure><img alt="Rock art photo by Rabah Al Shammary on Unsplash" src="https://cdn-images-1.medium.com/max/1024/0*MWZNUI_GoDUfvANG"></figure><p>This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me.</p>\n<img src="https://medium.com/_/stat?event=post.clientViewed&amp;referrerSource=full_rss&amp;postId=620c998688fe" width="1" height="1" alt="">\n',
+    '\n<figure><img alt="Photo by Someone on Unsplash" src="https://cdn-images-1.medium.com/max/1024/0*MHHHHHoDUfvANG"></figure><p>This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me. This is a mocked story written by me.</p>\n<img src="https://medium.com/_/stat?event=post.clientViewed&amp;referrerSource=full_rss&amp;postId=620c998688fe" width="1" height="1" alt="">\n',
   enclosure: {},
   categories: ["topic-one", "topic-two", "topic-three"],
 }
