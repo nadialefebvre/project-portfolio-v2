f0943b3159e7c9922856a52144428b70cf5f557f
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-01-24 20:45:54 +0100

    update socials

--- src/reference-data/socialIcons.tsx
+++ src/reference-data/socialIcons.tsx
@@ -1,15 +1,9 @@
 import React from "react"
 
-import {
-  FaLinkedinIn,
-  FaStackOverflow,
-  FaGithub,
-  FaMediumM,
-} from "react-icons/fa"
+import { FaLinkedinIn, FaGithub, FaMediumM } from "react-icons/fa"
 
 export const socialIcons: Map<string, JSX.Element> = new Map([
   ["Linkedin", <FaLinkedinIn />],
-  ["Stack Overflow", <FaStackOverflow />],
   ["GitHub", <FaGithub />],
   ["Medium", <FaMediumM />],
 ])
